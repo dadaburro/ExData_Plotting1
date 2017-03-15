c15e3b3e849faabc01a2d38eef5625dc5b915818
David Hernandez <dada.burro@gmail.com>
2017-03-15 15:15:40 -0600

    Create plot1.R

---
+++ plot1.R
@@ -0,0 +1,22 @@
+library(grDevices)
+
+#Read tables
+power <- read.table("household_power_consumption.txt", sep = ";", header = TRUE)
+
+#Used in all plotX.R to unfactor variables into numeric values
+unfactor <- function(x){as.numeric(levels(x)[as.integer(x)])}
+
+#Standardize time variables
+power$Date <- as.Date(power$Date , format = "%d/%m/%Y")
+power_sub <- power[grep("2007-02-0[1|2]", power$Date),]
+datetime <- paste(power_sub$Date, power_sub$Time)
+power_sub$Datetime <- as.POSIXct(datetime)
+
+#Unfactor variable to plot
+power_sub$Global_active_power <- unfactor(power_sub$Global_active_power)
+
+#Create png file and plot 
+png(filename = "plot1.png")
+with(power_sub,hist(Global_active_power, col = "red", main = paste("Global Active Power"), xlab = "Global Active Power (kilowatts)"))
+dev.off()
+
